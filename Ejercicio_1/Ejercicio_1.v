f7e884d94d435b255444497523c76bab638e3e40
EstephanZ246 <franciscoestephan@gmail.com>
2020-10-29 11:56:46 -0600

    Primer subida

---
+++ Ejercicio_1/Ejercicio_1.v
@@ -0,0 +1,22 @@
+module flipflopD(clk,reset,enable,D,Q);
+
+input wire clk;
+input wire reset;
+input wire enable;
+input wire D;
+output reg Q;
+
+
+always @(posedge clk , posedge reset)
+
+begin:flipflopD1bit
+ if (clk) begin
+  if (reset)  begin
+    Q<= 1'b0;
+  end
+  else if(enable) begin
+  Q <= D;
+  end
+  end
+end
+endmodule
---
+++ Ejercicio_1/Ejercicio_1_tb.v
@@ -0,0 +1,51 @@
+module flipflopD_tb();
+
+
+reg clk, reset, enable, D;
+wire Q;
+
+flipflopD f1(clk,reset,enable,D,Q);
+
+initial begin
+   #10 $display(" ");
+   $display(" ");
+   $display("flipflop tipo D");
+   $display("clk Reset Enable| D Q");
+   $display("________________|_____");
+   $monitor(" %b    %b     %b      %b %b  ", clk, reset, enable ,D ,Q);
+   end
+
+   always
+   #10 clk = ~clk;
+
+   initial begin
+ clk = 0;
+ enable = 1;
+ reset= 1;
+ D = 1;
+
+    /////////////////////
+  #10 enable = 1;
+  #10 reset = 1;
+  #10 D = 0;
+    /////////////////////
+  #10   enable = 1;
+  #10   reset = 0;
+  #10 D = 1;
+    ///////////////////
+  #10 enable = 1;
+  #10 reset = 0;
+  #10   D = 0;
+
+
+   $finish;
+
+  end
+
+
+  initial begin
+    $dumpfile("Ejercicio1_tb.vcd");
+    $dumpvars(0,flipflopD_tb);
+  end
+
+endmodule
